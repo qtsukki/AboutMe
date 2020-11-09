24936d2857d603757132145957df114ef86f9bf8
qtsukki <74160860+qtsukki@users.noreply.github.com>
2020-11-09 12:37:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// When clicking on the appropriate mousedown button, this message pops up in a window
+function alertMe(){
+  window.alert("I hope you're having a wonderful day :D");
+}
+// When the appropriate button is pressed, this message will show
+function myFunction() {
+document.getElementById("dog").innerHTML = "Arf! &#128062; ";
+}
+// When mousing over the picture of Mylo, it'll get bigger
+function bigImg(x) {
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+// Sets the normal size of the image
+function normalImg(x) {
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
